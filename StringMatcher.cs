607c4d11669e208ef677753c5a44f3c6c3934a3b
wkcn <wkcn@live.cn>
2017-10-30 13:59:55 +0800

    first commit

---
+++ StringMatcher.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMatcherNS
+{
+    public class StringMatcher
+    {
+        public const string ESCAPE_CHARACTER_ERROR = @"转义字符\后面必须存在字符，并且该字符为*,+,?,\之一";
+        /*
+         * pattern: 包含通配符的模式串
+         * value:   待匹配的字符串
+         * Return:  待匹配的字符串是否和模式串匹配
+         * 条件：
+         * (a) 通配符包扩 *, +, ?, 其中，* 匹配任意多个任意字符（可以为空），+匹配1个或以上任意字符，?匹配一个任意字符
+         * (b) 支持转义符号 ‘\’，\*、  \+、 \? 、\\ 分别匹配特殊字符 *, + ?, \
+           Exception: 这里定义，如果转移字符后不存在字符或者后面的字符不是*, + ?, \, 则抛出FormatException异常
+         *            \n, \t也算非法的模式串
+         */
+        public static bool Match(string pattern, string value)
+        {
+            // 首先检查是否存在\a等错误转义
+            CheckPatternFormat(pattern);
+            return MatchIn(pattern, value, 0, 0);
+        }
+
+        // 检查pattern是否合法（转义符是否正确）
+        private static void CheckPatternFormat(string pattern)
+        {
+            int i = 0;
+            while (i < pattern.Length)
+            {
+                if (pattern[i] == '\\')
+                {
+                    ++i; // 转义符号的下一个字符
+                    if (i >= pattern.Length) throw new FormatException(ESCAPE_CHARACTER_ERROR);
+                    char fc = pattern[i];
+                    if (fc != '\\' && fc != '*' && fc != '+' && fc != '?') throw new FormatException(ESCAPE_CHARACTER_ERROR);
+                }
+                ++i;
+            }
+        }
+
+        /*
+         * i: index of pattern
+         * j: index of value
+         */
+        private static bool MatchIn(string pattern, string value, int i, int j)
+        {
+            if (i < pattern.Length)
+            {
+
+                char pc = pattern[i];
+                // 当'*'是pattern的最后一个字符，且j >= value.Length时，字符串匹配
+                // 因此，将pc == '*'的判断放在条件j < value.Length外
+                if (pc == '*')
+                {
+                    for (int u = j; u <= value.Length; ++u)
+                    {
+                        if (MatchIn(pattern, value, i + 1, u)) return true;
+                    }
+                }
+
+                if (j < value.Length)
+                {
+                    if (pc == '+')
+                    {
+                        for (int u = j + 1; u <= value.Length; ++u)
+                        {
+                            if (MatchIn(pattern, value, i + 1, u)) return true;
+                        }
+                    }
+                    else if (pc == '?')
+                    {
+                        return MatchIn(pattern, value, i + 1, j + 1);
+                    }
+                    else if (pc == '\\')
+                    {
+                        // 转义字符
+                        // 这里定义，如果转移字符后不存在字符或者后面的字符不是*, + ?, \, 则抛出FormatException异常
+                        if (i + 1 >= pattern.Length) throw new FormatException(ESCAPE_CHARACTER_ERROR);
+                        char fc = pattern[i + 1];
+                        if (fc == '*' || fc == '+' || fc == '?' || fc == '\\')
+                        {
+                            if (value[j] == fc) return MatchIn(pattern, value, i + 2, j + 1);
+                            else return false;
+                        }
+                        else
+                        {
+                            throw new FormatException(ESCAPE_CHARACTER_ERROR);
+                        }
+                    }
+                    else
+                    {
+                        // 普通字符
+                        if (pc == value[j]) return MatchIn(pattern, value, i + 1, j + 1);
+                        else return false;
+                    }
+                }
+            }
+            return (i >= pattern.Length && j >= value.Length);
+        }
+    }
+}
---
+++ StringMatcherTest/StringMatcherTest.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringMatcherNS;
+using System.Text;
+
+namespace StringMatcherTest
+{
+    [TestClass]
+    public class StringMatcherTest
+    {
+        public const int TIMEOUT_MS = 5000; // 5s
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        // 简单测试
+        public void TestSimpleString()
+        {
+            Assert.AreEqual(StringMatcher.Match("", ""), true);
+            Assert.AreEqual(StringMatcher.Match("", "a"), false);
+            Assert.AreEqual(StringMatcher.Match("a", ""), false);
+            Assert.AreEqual(StringMatcher.Match("abc", "abc"), true);
+            Assert.AreEqual(StringMatcher.Match("adc", "abc"), false);
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        public void TestStar()
+        {
+            Assert.AreEqual(StringMatcher.Match("*", ""), true);
+            Assert.AreEqual(StringMatcher.Match("a*b", "ab"), true);
+            Assert.AreEqual(StringMatcher.Match("a*b", "awb"), true);
+            Assert.AreEqual(StringMatcher.Match("a*b", "awkb"), true);
+            Assert.AreEqual(StringMatcher.Match("a*b", "cawkb"), false);
+            Assert.AreEqual(StringMatcher.Match("a*b", "awkbc"), false);
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        public void TestAdd()
+        {
+            Assert.AreEqual(StringMatcher.Match("+", ""), false);
+            Assert.AreEqual(StringMatcher.Match("a+b", "ab"), false);
+            Assert.AreEqual(StringMatcher.Match("a+b", "awb"), true);
+            Assert.AreEqual(StringMatcher.Match("a+b", "awkb"), true);
+            Assert.AreEqual(StringMatcher.Match("a+b", "cawkb"), false);
+            Assert.AreEqual(StringMatcher.Match("a*b", "awkbc"), false);
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        public void TestQuesMask()
+        {
+            Assert.AreEqual(StringMatcher.Match("?", ""), false);
+            Assert.AreEqual(StringMatcher.Match("a?b", "ab"), false);
+            Assert.AreEqual(StringMatcher.Match("a?b", "awb"), true);
+            Assert.AreEqual(StringMatcher.Match("a?b", "awkb"), false);
+            Assert.AreEqual(StringMatcher.Match("a?b", "cawkb"), false);
+            Assert.AreEqual(StringMatcher.Match("a?b", "awkbc"), false);
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        public void TestEscape()
+        {
+            Assert.AreEqual(StringMatcher.Match(@"a\\b", @"a\b"), true);
+            Assert.AreEqual(StringMatcher.Match(@"a\*b", @"a*b"), true);
+            Assert.AreEqual(StringMatcher.Match(@"a\+b", @"a+b"), true);
+            Assert.AreEqual(StringMatcher.Match(@"a\?b", @"a?b"), true);
+            Assert.AreEqual(StringMatcher.Match(@"a\\?b", @"a\eb"), true);
+
+            Assert.AreEqual(StringMatcher.Match(@"a\\b", @"a*b"), false);
+            Assert.AreEqual(StringMatcher.Match(@"a\*b", @"a+b"), false);
+            Assert.AreEqual(StringMatcher.Match(@"a\+b", @"a?b"), false);
+            Assert.AreEqual(StringMatcher.Match(@"a\?b", @"a\b"), false);
+            Assert.AreEqual(StringMatcher.Match(@"a\\?b", @"a\ec"), false);
+
+            // 输入异常检查
+            bool throwed = false;
+            try
+            {
+                // 转义符号后面没有字符
+                StringMatcher.Match(@"\", @"\");
+            }
+            catch (FormatException e)
+            {
+                Assert.AreEqual(e.Message, StringMatcher.ESCAPE_CHARACTER_ERROR);
+                throwed = true;
+            }
+
+            if (!throwed)
+                Assert.Fail("No exception was thrown.");
+
+            throwed = false;
+            try
+            {
+                // 转义符号后面的字符不是*,+,?,\
+                StringMatcher.Match(@"\a", @"\");
+            }
+            catch (FormatException e)
+            {
+                Assert.AreEqual(e.Message, StringMatcher.ESCAPE_CHARACTER_ERROR);
+                throwed = true;
+            }
+
+            if (!throwed)
+                Assert.Fail("No exception was thrown.");  
+
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        public void TestSpecial()
+        {
+            Assert.AreEqual(StringMatcher.Match(@"*?Mc\\l", @"ctUVUdLMc\l"), true);
+        }
+
+        // 测试样本指标(近似值)：
+        const int STR_MAX_LENGTH = 20; // 字符串长度： 0 ~ STR_MAX_LENGTH
+        const int NUM_TRUE_SAMPLE = 1000; // 匹配的样例
+        const int NUM_FALSE_SAMPLE = 1000; // 不匹配的样例
+        const int NUM_STAR_SAMPLE = 500; // *
+        const int NUM_ADD_SAMPLE = 500; // +
+        const int NUM_QUES_SAMPLE = 500; // ?
+        const int NUM_ESCAPE_RIGHT = 200; // 正确的转义
+        const int NUM_ESCAPE_WRONG = 10; // 错误的转义
+        const int NUM_GOOD_SAMPLE = 500;
+
+        enum RECORD_IDX
+        {
+            NULL, TRUE, FALSE, STAR, ADD, QUES, ESCAPE_RIGHT, ESCAPE_WRONG, GOOD
+        }
+
+        static Random random = new Random(3939);
+        static string wildcards = @"*+?\";
+
+        private char get_random_alpha()
+        {
+            if (random.Next(0, 2) == 0)
+                return (char)random.Next('a', 'z' + 1);
+            return (char)random.Next('A', 'Z' + 1);
+        }
+
+        private int add_random_string(ref string value)
+        {
+            int w = random.Next(0, 5);
+            for (int u = 0; u < w; ++u)
+            {
+                value += get_random_alpha();
+            }
+            return w;
+        }
+
+        private int add_special_string(ref string value, ref string buffer, ref bool true_sample)
+        {
+            if (buffer.Length == 0) return 0;
+            bool has_more = false;
+            int lower = 0;
+            foreach (char c in buffer)
+            {
+                if (c == '?'){
+                    ++lower;
+                }
+                else if (c == '+')
+                {
+                    ++lower;
+                    has_more = true;
+                }else if (c == '*') has_more = true;
+            }
+            int delta = random.Next(lower > 2 ? -2 : -lower, 3);
+            int w = lower + delta;
+            for (int u = 0; u < w; ++u)
+            {
+                value += get_random_alpha();
+            }
+            if (delta < 0) true_sample = false;
+            if (delta > 0 && !has_more) true_sample = false;
+            buffer = "";
+            return w;
+        }
+
+        private void RandomTestOnce(int[] record) 
+        {
+            int len = random.Next(0, 20);
+            string pattern = "";
+            string value = "";
+            bool true_sample = true;
+            bool escape_wrong = false;
+            bool star_sample = false;
+            bool add_sample = false;
+            bool ques_sample = false;
+            bool escape_sample = false;
+
+            string buffer = ""; // 用于记录特殊符号*+?
+            for (int i = 0; i < len; ++i)
+            {
+                int r = random.Next(0, 100);
+                if (0 <= r && r < 10)
+                {
+                    // *
+                    pattern += '*';
+                    buffer += '*';
+                    star_sample = true;
+                }
+                else if (10 <= r && r < 20)
+                {
+                    // +
+                    pattern += '+';
+                    buffer += '+';
+                    add_sample = true;
+                }
+                else if (20 <= r && r < 30)
+                {
+                    // ?
+                    pattern += '?';
+                    buffer += '?';
+                    ques_sample = true;
+                }
+                else if (r >= 30)
+                {
+                    add_special_string(ref value, ref buffer, ref true_sample);
+                    if (30 <= r && r < 40)
+                    {
+                        // escape
+                        // 转义符号测试
+                        escape_sample = true;
+                        pattern += '\\';
+                        char wildcard = wildcards[random.Next(0, wildcards.Length)];
+                        if (random.Next(0, 10) != 4)
+                        {
+                            // 生成pattern合法的转义符测试样例
+                            pattern += wildcard;
+                            if (random.Next(0, 10) < 8)
+                            {
+                                // 生成value与pattern对应元素匹配的样例
+                                value += wildcard;
+                            }
+                            else
+                            {
+                                // 生成转义符被忽略的样例, value错误
+                                add_random_string(ref value);
+                                true_sample = false;
+                            }
+                        }
+                        else
+                        {
+                            // 生成错误的转义符样例，pattern错误
+                            pattern += 'a'; // exception test
+                            value += 'a';
+                            escape_wrong = true;
+                        }
+                    }
+                    else
+                    {
+                        char c = get_random_alpha();
+                        pattern += c;
+                        if (random.Next(0, 10) < 7){
+                            // 生成正确的样例 
+                            value += c;
+                        }
+                        else if (random.Next(0, 30) > 28)
+                        {
+                            // value比pattern少一个字符
+                            true_sample = false;
+                        }
+                        else{
+                            char c2 = get_random_alpha();
+                            if (c2 != c)
+                            {
+                                // value和pattern存在至少一个字符不匹配
+                                true_sample = false;
+                            }
+                            value += c2;
+                        }
+                    }
+                }
+            }
+            add_special_string(ref value, ref buffer, ref true_sample);
+
+            if (escape_wrong) true_sample = false;
+
+            try
+            {
+                Console.WriteLine("{0}, {1}, [ANSWER]: {2}, exception: {3}", pattern, value, true_sample, escape_wrong);
+                Assert.AreEqual(StringMatcher.Match(pattern, value), true_sample);
+                Assert.AreEqual(escape_wrong, false);
+            }
+            catch (FormatException e)
+            {
+                Assert.AreEqual(escape_wrong, true);
+                Assert.AreEqual(e.Message, StringMatcher.ESCAPE_CHARACTER_ERROR);
+            }
+
+            // 记录测试样例类型
+            if (true_sample) ++record[(int)RECORD_IDX.TRUE];
+            else ++record[(int)RECORD_IDX.FALSE];
+
+            if (star_sample) ++record[(int)RECORD_IDX.STAR];
+            if (add_sample) ++record[(int)RECORD_IDX.ADD];
+            if (ques_sample) ++record[(int)RECORD_IDX.QUES];
+
+            if (true_sample && len > 5)
+            {
+                ++record[(int)RECORD_IDX.GOOD];
+            }
+            if (escape_sample)
+            {
+                if (escape_wrong) ++record[(int)RECORD_IDX.ESCAPE_WRONG];
+                else ++record[(int)RECORD_IDX.ESCAPE_RIGHT];
+            }
+        }
+
+        [TestMethod]
+        [Timeout(TIMEOUT_MS)]
+        // 随机测试
+        public void TestRandomString()
+        {
+
+            int[] record = new int[9];
+            while(
+                record[(int)RECORD_IDX.TRUE] < NUM_TRUE_SAMPLE ||
+                record[(int)RECORD_IDX.FALSE] < NUM_FALSE_SAMPLE ||
+                record[(int)RECORD_IDX.STAR] < NUM_STAR_SAMPLE ||
+                record[(int)RECORD_IDX.ADD] < NUM_ADD_SAMPLE ||
+                record[(int)RECORD_IDX.QUES] < NUM_QUES_SAMPLE ||
+                record[(int)RECORD_IDX.ESCAPE_RIGHT] < NUM_ESCAPE_RIGHT ||
+                record[(int)RECORD_IDX.ESCAPE_WRONG] < NUM_ESCAPE_WRONG ||
+                record[(int)RECORD_IDX.GOOD] < NUM_GOOD_SAMPLE
+                )
+            {
+                RandomTestOnce(record);
+            }
+
+            Console.WriteLine("Test OK, TRUE_SAMPLE:{0}, FALSE_SAMPLE:{1}, STAR_SAMPLE:{2}, ADD_SAMPLE:{3}, QUES_SAMPLE:{4}, ESCAPE_RIGHT:{5}, ESCAPE_WRONG:{6}, GOOD_SAMPLE:{7}",
+                record[(int)RECORD_IDX.TRUE],
+                record[(int)RECORD_IDX.FALSE],
+                record[(int)RECORD_IDX.STAR],
+                record[(int)RECORD_IDX.ADD],
+                record[(int)RECORD_IDX.QUES],
+                record[(int)RECORD_IDX.ESCAPE_RIGHT],
+                record[(int)RECORD_IDX.ESCAPE_WRONG],
+                record[(int)RECORD_IDX.GOOD]);
+
+        }
+
+    }
+}
